bcdd0664db70d6505b79ad2acd93e791a8e5ee97
Natan Matos <natanmatos_18@hotmail.com>
2024-10-06 21:16:50 -0300

    :rocket: Create function to remove elements from Binary Tree

--- binary_tree/binary_tree.c
+++ binary_tree/binary_tree.c
@@ -1,4 +1,5 @@
 #include "../binary_node/binary_node.c"
+#include "../errors/error_message.c"
 #include <stdlib.h>
 
 typedef struct BinaryTree {
@@ -46,4 +47,68 @@ void insert_element(int value, BinaryTree *binary_tree) {
 	}
 
 	last_element->left_node = node;
+}
+
+void remove_element(int value, BinaryTree *binary_tree)
+{
+	BinaryNode *current_element = binary_tree->root;
+	BinaryNode *previous_element = current_element;
+
+	while (current_element->value != value) {
+		if (current_element->left_node && current_element->value > value) {
+			previous_element = current_element;
+			current_element = current_element->left_node;
+			continue;
+		}
+
+		if (current_element->right_node && current_element->value < value) {
+			previous_element = current_element;
+			current_element = current_element->right_node;
+			continue;
+		}
+
+		exit_with_error_message("Element not found.");
+	}
+
+	if (current_element == binary_tree->root) {
+		if (!current_element->left_node && !current_element->right_node) {
+			binary_tree->root = NULL;
+			free(current_element);
+			return;
+		}
+
+		if (current_element->left_node) {
+			binary_tree->root = current_element->left_node;
+			binary_tree->root->right_node = current_element->right_node;
+			free(current_element);
+			return;
+		}
+
+		binary_tree->root = current_element->right_node;
+		free(current_element);
+		return;
+	}
+
+	if (previous_element->left_node == current_element) {
+		if (current_element->right_node) {
+			previous_element->left_node = current_element->right_node;
+			previous_element->left_node->left_node = current_element->left_node;
+			free(current_element);
+			return;
+		}
+
+		previous_element->left_node = current_element->left_node;
+		free(current_element);
+		return;
+	}
+
+	if (current_element->left_node) {
+		previous_element->right_node = current_element->left_node;
+		previous_element->right_node->right_node = current_element->right_node;
+		free(current_element);
+		return;
+	}
+
+	previous_element->right_node = current_element->right_node;
+	free(current_element);
 }
